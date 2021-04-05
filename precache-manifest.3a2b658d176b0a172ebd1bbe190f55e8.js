68f01e426cf8099dfbe4a7cd44338709d3d90256
ahmadbakhshi <bakhshi.ahmad@hotmail.com>
2021-04-04 21:26:05 -0600

    Updates

---
+++ precache-manifest.3a2b658d176b0a172ebd1bbe190f55e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7647c7e300280166fef80ecaf378f1cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c4bf87eb0a99d0c17b3",
+    "url": "./static/js/2.068b97b4.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "./static/js/2.068b97b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af33d5c3a9f4ef4c3491",
+    "url": "./static/js/main.43c7dfd2.chunk.js"
+  },
+  {
+    "revision": "3e97f8526ca8dbbaf62e",
+    "url": "./static/js/runtime-main.5efb44b1.js"
+  }
+]);
